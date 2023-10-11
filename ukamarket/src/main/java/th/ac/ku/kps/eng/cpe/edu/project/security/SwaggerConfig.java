21f2c5cda436ffb33ff4c50a89c29c84d7e15a56
Peerawat Wongmek <78299267+peerawat2645@users.noreply.github.com>
2023-10-11 14:14:19 +0700

    Update SwaggerConfig.java

--- ukamarket/src/main/java/th/ac/ku/kps/eng/cpe/edu/project/security/SwaggerConfig.java
+++ ukamarket/src/main/java/th/ac/ku/kps/eng/cpe/edu/project/security/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("th.ac.ku.kps.eng.cpe.ds.project.api.service")) // Change to your base package
+                .apis(RequestHandlerSelectors.basePackage("th.ac.ku.kps.eng.cpe.edu.project.api.service")) // Change to your base package
                 .build()
                 .securitySchemes(Arrays.asList(apiKey()));
     }
