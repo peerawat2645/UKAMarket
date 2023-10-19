1792b760770da4ddec38e35e36fa1b305be76010
Jiraporn <jiraporn.nie@ku.th>
2023-10-19 15:33:57 +0700

    update login

--- uka_project/lib/ui/screen/login_screen.dart
+++ uka_project/lib/ui/screen/login_screen.dart
@@ -14,8 +14,11 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passController = TextEditingController();
+  final GlobalKey<FormState> _formLogin = GlobalKey<FormState>();
 
-  void _handleLogin() {
+  bool passTogale = true;
+
+  void _handleLogin() async {
     final String username = _emailController.text;
     final String password = _passController.text;
 
@@ -28,14 +31,19 @@ class _LoginScreenState extends State<LoginScreen> {
         print('login Successful: $result');
         int userId = result['body']['userId'];
         print(userId);
-        Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-                builder: (BuildContext context) => MainPage(userId: userId)));
+        if (_formLogin.currentState!.validate()) {
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => MainPage(userId: userId)));
+        }
       }
     }).catchError((error) {
       print('POST Failed: $error');
     });
+    if (_formLogin.currentState!.validate()) {
+      print('Succ');
+    }
   }
 
   @override
@@ -80,162 +88,192 @@ class _LoginScreenState extends State<LoginScreen> {
                         ),
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 50),
-                          child: Column(
-                            textDirection: TextDirection.ltr,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'เข้าสู่ระบบ',
-                                style: TextStyle(
-                                  color: const Color(0xFF040D12),
-                                  fontSize: 20.sp,
-                                  fontFamily: 'BaiJamjuree',
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              SizedBox(
-                                height: 25.h,
-                              ),
-                              TextField(
-                                controller: _emailController,
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: const Color(0xFF393939),
-                                  fontSize: 13.sp,
-                                  fontFamily: 'BaiJamjuree',
-                                  fontWeight: FontWeight.w400,
-                                ),
-                                decoration: InputDecoration(
-                                  labelText: 'ชื่อผู้ใช้',
-                                  labelStyle: TextStyle(
-                                    color: const Color(0xFF040D12),
-                                    fontSize: 15.sp,
-                                    fontFamily: 'BaiJamjuree',
-                                    fontWeight: FontWeight.w100,
-                                  ),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(10)),
-                                    borderSide: BorderSide(
-                                      width: 1.w,
-                                      color: const Color(0xFF837E93),
-                                    ),
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(10)),
-                                    borderSide: BorderSide(
-                                      width: 1.w,
-                                      color: const Color(0xFF040D12),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 30.h,
-                              ),
-                              TextField(
-                                controller: _passController,
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: const Color(0xFF393939),
-                                  fontSize: 13.sp,
-                                  fontFamily: 'BaiJamjuree',
-                                  fontWeight: FontWeight.w400,
-                                ),
-                                decoration: InputDecoration(
-                                  labelText: 'รหัสผ่าน',
-                                  labelStyle: TextStyle(
-                                    color: const Color(0xFF040D12),
-                                    fontSize: 15.sp,
-                                    fontFamily: 'BaiJamjuree',
-                                    fontWeight: FontWeight.w300,
-                                  ),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(10)),
-                                    borderSide: BorderSide(
-                                      width: 1.w,
-                                      color: const Color(0xFF837E93),
-                                    ),
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(10)),
-                                    borderSide: BorderSide(
-                                      width: 1.w,
-                                      color: const Color(0xFF040D12),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 25.h,
-                              ),
-                              ClipRRect(
-                                borderRadius:
-                                    const BorderRadius.all(Radius.circular(10)),
-                                child: SizedBox(
-                                  width: 329.w,
-                                  height: 56.h,
-                                  child: ElevatedButton(
-                                    onPressed: _handleLogin,
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: const Color(0xFF435334),
-                                    ),
-                                    child: Text(
-                                      'เข้าสู่ระบบ',
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 15.sp,
-                                        fontFamily: 'BaiJamjuree',
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 15.h,
-                              ),
-                              Row(
+                          child: Form(
+                              key: _formLogin,
+                              child: Column(
+                                textDirection: TextDirection.ltr,
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Text(
-                                    'ยังไม่มีบัญชี?',
+                                    'เข้าสู่ระบบ',
                                     style: TextStyle(
-                                      color: const Color(0xFF837E93),
-                                      fontSize: 14.sp,
+                                      color: const Color(0xFF040D12),
+                                      fontSize: 20.sp,
                                       fontFamily: 'BaiJamjuree',
                                       fontWeight: FontWeight.w500,
                                     ),
                                   ),
                                   SizedBox(
-                                    width: 2.5.w,
+                                    height: 25.h,
                                   ),
-                                  InkWell(
-                                    onTap: () {
-                                      widget.controller.animateToPage(1,
-                                          duration:
-                                              const Duration(milliseconds: 500),
-                                          curve: Curves.ease);
-                                    },
-                                    child: Text(
-                                      'ลงทะเบียน',
-                                      style: TextStyle(
-                                        color: const Color(0xFF9EB384),
-                                        fontSize: 14.sp,
+                                  TextFormField(
+                                    controller: _emailController,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: const Color(0xFF393939),
+                                      fontSize: 13.sp,
+                                      fontFamily: 'BaiJamjuree',
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                    decoration: InputDecoration(
+                                      labelText: 'ชื่อผู้ใช้',
+                                      labelStyle: TextStyle(
+                                        color: const Color(0xFF040D12),
+                                        fontSize: 15.sp,
                                         fontFamily: 'BaiJamjuree',
-                                        fontWeight: FontWeight.w500,
+                                        fontWeight: FontWeight.w100,
+                                      ),
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: const BorderRadius.all(
+                                            Radius.circular(10)),
+                                        borderSide: BorderSide(
+                                          width: 1.w,
+                                          color: const Color(0xFF837E93),
+                                        ),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: const BorderRadius.all(
+                                            Radius.circular(10)),
+                                        borderSide: BorderSide(
+                                          width: 1.w,
+                                          color: const Color(0xFF040D12),
+                                        ),
+                                      ),
+                                      border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(10)),
+                                          borderSide: BorderSide(
+                                            width: 1.w,
+                                            color: Color(0xFF040D12),
+                                          )),
+                                    ),
+                                    validator: (value) {
+                                      if (value!.isEmpty) {
+                                        return 'กรอกชื่อผู้ใช้';
+                                      }
+                                    },
+                                  ),
+                                  SizedBox(
+                                    height: 30.h,
+                                  ),
+                                  TextFormField(
+                                    controller: _passController,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: const Color(0xFF393939),
+                                      fontSize: 13.sp,
+                                      fontFamily: 'BaiJamjuree',
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                    validator: (value) {
+                                      if (value!.isEmpty)
+                                        return 'กรุณากรอกรหัสผ่าน';
+                                      if (value.length <= 8) {
+                                        return "รหัสผ่านต้องมีอย่างน้อย 8 อักขระ";
+                                      }
+                                      return null;
+                                    },
+                                    obscureText: passTogale,
+                                    decoration: InputDecoration(
+                                        labelText: 'รหัสผ่าน',
+                                        labelStyle: TextStyle(
+                                          color: const Color(0xFF040D12),
+                                          fontSize: 15.sp,
+                                          fontFamily: 'BaiJamjuree',
+                                          fontWeight: FontWeight.w300,
+                                        ),
+                                        enabledBorder: OutlineInputBorder(
+                                          borderRadius: const BorderRadius.all(
+                                              Radius.circular(10)),
+                                          borderSide: BorderSide(
+                                            width: 1.w,
+                                            color: const Color(0xFF837E93),
+                                          ),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                          borderRadius: const BorderRadius.all(
+                                              Radius.circular(10)),
+                                          borderSide: BorderSide(
+                                            width: 1.w,
+                                            color: const Color(0xFF040D12),
+                                          ),
+                                        ),
+                                        border: OutlineInputBorder(
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(10)),
+                                            borderSide: BorderSide(
+                                              width: 1.w,
+                                              color: Color(0xFF040D12),
+                                            ))),
+                                  ),
+                                  SizedBox(
+                                    height: 25.h,
+                                  ),
+                                  ClipRRect(
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(10)),
+                                    child: SizedBox(
+                                      width: 329.w,
+                                      height: 56.h,
+                                      child: ElevatedButton(
+                                        onPressed: _handleLogin,
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor:
+                                              const Color(0xFF435334),
+                                        ),
+                                        child: Text(
+                                          'เข้าสู่ระบบ',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 15.sp,
+                                            fontFamily: 'BaiJamjuree',
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
                                       ),
                                     ),
                                   ),
+                                  SizedBox(
+                                    height: 15.h,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        'ยังไม่มีบัญชี?',
+                                        style: TextStyle(
+                                          color: const Color(0xFF837E93),
+                                          fontSize: 14.sp,
+                                          fontFamily: 'BaiJamjuree',
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 2.5.w,
+                                      ),
+                                      InkWell(
+                                        onTap: () {
+                                          widget.controller.animateToPage(1,
+                                              duration: const Duration(
+                                                  milliseconds: 500),
+                                              curve: Curves.ease);
+                                        },
+                                        child: Text(
+                                          'ลงทะเบียน',
+                                          style: TextStyle(
+                                            color: const Color(0xFF9EB384),
+                                            fontSize: 14.sp,
+                                            fontFamily: 'BaiJamjuree',
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 15.h,
+                                  ),
                                 ],
-                              ),
-                              SizedBox(
-                                height: 15.h,
-                              ),
-                            ],
-                          ),
+                              )),
                         ),
                       ],
                     );
